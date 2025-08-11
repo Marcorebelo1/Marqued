56ad825c24ebbb384b70a78c20d3f6bb8a42dcff
Marcorebelo1 <marcorebelo2062005@gmail.com>
2025-08-11 11:51:54 +0100

    Initial Configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Parkinsans } from "next/font/google";
+
+const parkinsans = Parkinsans({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${parkinsans.className} antialiased`}>
         {children}
       </body>
     </html>
